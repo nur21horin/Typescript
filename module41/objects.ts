c6798c72face278e974789364898def28ed89e16
Nur Mohammod <nurm98371@gmail.com>
2026-02-17 00:07:19 +0600

    add:

--- module41/objects.ts
+++ module41/objects.ts
@@ -16,3 +16,22 @@ class Animal {
 const dog = new Animal("Robi", "DOg", "GhayFHau");
 
 dog.makeSound();
+
+class Animal1 {
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string,
+  ) {}
+
+  public MakeSound() {
+    console.log(`My name is ${this.name} and my sound is ${this.sound}`);
+  }
+  public Species() {
+    console.log(`My species is ${this.species}`);
+  }
+}
+
+const Billu = new Animal1("Billu", "Hykka", "Mew Mew");
+
+Billu.Species();
