69905feca4cc5bb30fff14ecd6df727c3fd95dd2
Nur Mohammod <nurm98371@gmail.com>
2026-02-18 00:00:19 +0600

    Add

--- questionmark.ts
+++ questionmark.ts
@@ -66,3 +66,9 @@ const manush3: manush2 = {
 
 const nur = manush3.name;
 console.log(nur);
+
+function throwEror(messsage: string): never {
+  throw new Error(messsage);
+}
+
+console.log(throwEror("Nur Eroro khaice"));
