ba4e5cff54497bf03bc88ad3d81dd4fed2d99b70
Nur Mohammod <nurm98371@gmail.com>
2026-02-18 19:43:54 +0600

    Add

--- Generic.ts
+++ Generic.ts
@@ -6,3 +6,13 @@ function identity(value: string): string {
 
 console.log(identity("Shofiq Harce"));
 
+function identity(value:number):number{
+    return value;
+}
+console.log(identity(39+3))
+
+function identity(value:number,value1:number):number{
+    return value+value1;
+}
+
+console.log(identity(390,383));
