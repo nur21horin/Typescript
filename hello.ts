d5bf297a88e8ceb276b19b95a10aa37dfd1dbfe3
Nur Mohammod <nurm98371@gmail.com>
2026-02-20 11:20:52 +0600

    Add

---
+++ hello.ts
@@ -0,0 +1,12 @@
+function greet(name:string):string{
+    return `Hello , ${name}`;
+}
+const message:string=greet("World");
+console.log(message)
+
+function ReturnGReeting(name:string):string{
+    return `Hi , ${name}`;
+}
+const messageReturn:string=ReturnGReeting("Nur");
+
+console.log(messageReturn)
