b04498ab11f9f9b59840b95f8c25fc5370d96480
Nur Mohammod <nurm98371@gmail.com>
2026-02-20 12:11:04 +0600

    add

--- DataType.ts
+++ DataType.ts
@@ -6,10 +6,31 @@ const name: string = "Nur Mohammod";
 const score: number = 300;
 const isLoading: boolean = true;
 const Exam: number[] = [200, 344, 50, 30];
-console.log(Exam)
+console.log(Exam);
 Exam[2] = 399;
 console.log(Exam);
 
 //Type inference
-const name2="Nur"
-const score2=300
+const name2 = "Nur";
+const score2 = 300;
+
+//Object Type
+const user = {
+  name: "Nur",
+  age: 30,
+  isAdmin: true,
+};
+console.log(user);
+user.name = "AagdumBaagDum";
+console.log(user);
+
+function isUserName(name: string): boolean {
+  return name === user.name;
+}
+if (isUserName("AaddumBaagDum")) {
+  console.log("Yes");
+} else {
+  console.log("No");
+  console.log(user.name)
+
+}
