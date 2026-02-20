000133cc89d42ecda58b1101febd26edc79e5a4e
Nur Mohammod <nurm98371@gmail.com>
2026-02-20 11:25:08 +0600

    Add

--- hello.ts
+++ hello.ts
@@ -1,18 +1,24 @@
-function greet(name:string):string{
-    return `Hello , ${name}`;
+function greet(name: string): string {
+  return `Hello , ${name}`;
 }
-const message:string=greet("World");
-console.log(message)
+const message: string = greet("World");
+console.log(message);
 
-function ReturnGReeting(name:string):string{
-    return `Hi , ${name}`;
+function ReturnGReeting(name: string): string {
+  return `Hi , ${name}`;
 }
-const messageReturn:string=ReturnGReeting("Nur");
+const messageReturn: string = ReturnGReeting("Nur");
 
-console.log(messageReturn)
+console.log(messageReturn);
 
-function Welcome(value:string):string{
-    return `Hey What about You ${value}`;
+function Welcome(value: string): string {
+  return `Hey What about You ${value}`;
 }
-const welCome:string=Welcome("nur");
-console.log(welCome)
+const welCome: string = Welcome("nur");
+console.log(welCome);
+
+function Country(value: string): string {
+  return `What is Your Country ${value}`;
+}
+const CountryName: string = Country("Bangladesh");
+console.log(CountryName);
