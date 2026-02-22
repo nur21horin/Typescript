76692c903e45739e32cd1dd4a39e4f10d9f5bca9
Nur Mohammod <nurm98371@gmail.com>
2026-02-22 22:40:58 +0600

    Add

--- OOP.ts
+++ OOP.ts
@@ -1,2 +1,22 @@
 console.log("Welcome in OOP")
 
+console.log("Four pillars Encapsulation, Abstraction, Inheritance, Polymorphism")
+
+//class
+class Person{
+    nams:string;
+    age:number;
+
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age;
+    }
+    greet(){
+        return `Thank You ${this.name}`;
+    }
+}
+
+//Object
+
+const p1=new Person("Nur",23);
+console.log(p1.greet())
